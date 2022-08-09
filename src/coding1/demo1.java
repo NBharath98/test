bc0c7a406d4e13771e41a817170b8a9d7db47f6a
Nakkila <bharath-kumar.nakkila@capgemini.com>
2022-08-09 17:20:21 +0530

    first commiting

---
+++ src/coding1/demo1.java
@@ -0,0 +1,12 @@
+package coding1;
+
+public class demo1 {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=30;
+	System.out.println((a>b) ? ((a>c)?a:c): ((b>c)?b:c));
+	
+}
+
+}
---
+++ src/coding1/demo2.java
@@ -0,0 +1,14 @@
+package coding1;
+
+public class demo2 {
+public static void main(String[] args)
+{
+	int valueA=21;
+	int valueB=6;
+	int valueC=3;	
+	int result1=valueA-valueB/valueC;
+	int result2=(valueA-valueB)/valueC;
+	System.out.println(result1);
+	System.out.println(result2);
+}
+}
